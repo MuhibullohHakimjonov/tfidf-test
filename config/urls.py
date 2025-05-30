bdc295b5e326102a7fea1fa2d4d718618d8c145a
MuhabillohHakimjonov <mukhabillo@gmail.com>
2025-05-30 20:04:18 +0500

    v1.3 updated template

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	path('', include('tfidf.urls')),
+	path('api/', include('tfidf.urls')),
 	path('', TemplateView.as_view(template_name="index.html"), name="home"),
 ]
 
