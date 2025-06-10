6f3afc9d738a7b1c30954cbf828bdcd3facd5094
MuhabillohHakimjonov <mukhabillo@gmail.com>
2025-06-10 22:51:13 +0500

    v2.0 updated frontend

--- frontend/src/components/DocumentDetail.vue
+++ frontend/src/components/DocumentDetail.vue
@@ -10,7 +10,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
-import axios from '../api.js';
+import axios from 'axios';
 
 const route = useRoute();
 const content = ref('');
@@ -22,12 +22,12 @@ onMounted(async () => {
   error.value = null;
   try {
     const response = await axios.get(`documents/${route.params.id}/`);
-    console.log(import.meta.env.VITE_API_URL);
+    console.log(response, "is good");
     content.value = response.data.content; // Access the content field
   } catch (err) {
     error.value = 'Не удалось загрузить содержимое документа.';
     console.error(err);
-    console.log(import.meta.env.VITE_API_URL);
+    console.log(response);
   } finally {
     loading.value = false;
     console.log(import.meta.env.VITE_API_URL);
