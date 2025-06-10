aa58cf76436fade07269afe45029cdcbdf83ec77
MuhabillohHakimjonov <mukhabillo@gmail.com>
2025-06-10 12:14:51 +0500

    v2.0 fixed admin bug

--- tfidf/admin.py
+++ tfidf/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from .models import Document, Collection
 
 
-@admin.site.register(Document)
+@admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
 	list_display = ['id', 'user', 'name', 'word_count']
 	list_display_links = ['id', 'user']
 
 
-@admin.site.register(Collection)
+@admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
 	list_display = ['id', 'user', 'name', 'created_at']
 	list_display_links = ['id', 'user']
--- user/admin.py
+++ user/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import CustomUser
 
 
-@admin.site.register(CustomUser)
+@admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
 	list_display = ['id', 'email', 'username', 'is_active', 'is_staff', 'date_joined']
 	list_display_links = ['id', 'email']
